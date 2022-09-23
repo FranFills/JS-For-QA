1ae6974743706cb4048c16d660a55ce6a1db3285
fobabire <fobabire@ecobank.com>
2022-09-23 17:01:15 +0100

    JS for QA

---
+++ JS Coding Challenge Fisayo & Rita/jsCodeChallenge5.js
@@ -0,0 +1,7 @@
+// create a function that reverses an array
+
+//const arrayNumbers = [1, 2, 3, 4, 5, 10]
+const arrayNumbers = ['Rita', 'Fisayo', 'Bisi', 'Kemi']
+
+arrayNumbers.reverse()
+console.log(arrayNumbers)
